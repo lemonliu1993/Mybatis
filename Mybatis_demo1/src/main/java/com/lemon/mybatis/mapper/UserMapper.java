f1e51eb25e8c3a4a8e65f43d8ba389aa3eff0ce0
lemonliu1993 <2305247091@qq.com>
2023-11-26 19:13:30 +0800

    mybatis: 创建Mybatis映射

--- Mybatis_demo1/src/main/java/com/lemon/mybatis/mapper/UserMapper.java
+++ Mybatis_demo1/src/main/java/com/lemon/mybatis/mapper/UserMapper.java
@@ -5,4 +5,17 @@ package com.lemon.mybatis.mapper;
  */
 public interface UserMapper {
 
+    /**
+     * Mybatis 面向接口编程的两个一致：
+     * 1、映射文件的namespace要和mapper接口的全类名保持一致
+     * 2、映射文件中SQL语句的id要和mapper接口中的方法名一致
+     *
+     * 表 —— 实体类 —— mapper接口 —— 映射文件
+     */
+
+    /**
+     * 添加用户信息
+     */
+    int insertUser();
+
 }
