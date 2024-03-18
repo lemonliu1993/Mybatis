896c7c6d17cbff7db2182ea2bb85c5b76849f910
lemonliu1993 <2305247091@qq.com>
2024-03-18 08:09:40 +0800

    Mybatis_demo1 搭建环境

---
+++ Mybatis_demo1/src/main/java/com/lemon/mybatis/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.lemon.mybatis.mapper;
+
+/**
+ * Created by lemoon on 2024/3/17 11:05
+ */
+public interface UserMapper {
+
+    /**
+     * 添加用户信息
+     * @return
+     */
+    int insertUser();
+}
